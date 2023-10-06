87842a90616561d5361c8c959dd09dca76ef24e6
steph-ayo <popoolastephen19@gmail.com>
2023-10-06 22:08:42 +0100

    app-update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
   { id: 3, description: "Charger", quantity: 12, packed: false },
+  { id: 4, description: "Bottled water", quantity: 2, packed: false },
+  { id: 5, description: "Books", quantity: 1, packed: true },
 ];
 
 export default function App() {
